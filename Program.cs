79024be9c63b47b2f09ecb7ab69d5e6671c6abba
Xaglez <paulomontealtomac@gmail.com>
2024-02-19 13:18:22 -0300

    initial-commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+
+class Program{
+    public static void Main(string[] args){
+        Console.WriteLine("Insira o temanho do fibonacci");
+        try{
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++){
+            Console.Write(" "+ fibo(i));
+        }
+
+        }catch (Exception ex){
+            Console.WriteLine("Insira um número válido, Erro: " + ex.Message);
+        }
+    }
+
+    public static int fibo(int n){
+        if (n == 0){
+            return 0;
+        }
+        else if(n == 1){
+            return 1;
+        }
+        else{
+            return fibo(n -1) + fibo(n - 2);
+        }
+    }
+}
